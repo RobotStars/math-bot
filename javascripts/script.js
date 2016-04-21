8d1334d17fb71b638f160d930b5b0971ad16d3d5
Alexander Mann <mannimal09@gmail.com>
2016-04-20 21:29:01 -0400

    Update script.js

--- javascripts/script.js
+++ javascripts/script.js
@@ -5,15 +5,48 @@ $(document).ready(function(){
             if (number.length > 15) {
                 number = "";
                 totaldiv.text("Err");
-            }}};
-    var number = "";
+            }
+        } 
+    };
+	var number = "";
     var newnumber = "";
     var operator = "";
     var totaldiv = $("#total");
     totaldiv.text("0");
-    $("#numbers a").not("#clear,#clearall").click(function(){
-        number += $(this).html();
-        totaldiv.text(number);
-        testNumLength(number);
+    $("#numbers > a").not("#clear,#clearall").click(function(){
+		number += $(this).text();
+		totaldiv.text(number);
+		testNumLength(number);
+    });
+    $("#operators > a").not("#equals").click(function(){
+		operator = $(this).text();
+		newnumber = number;
+		number = "";
+		totaldiv.text("0");
+    });
+    $("#clear,#clearall").click(function(){
+		number = "";
+		totaldiv.text("0");
+		if ($(this).attr("id") === "clearall") {
+			newnumber = "";
+		}
+    });
+    $("#equals").click(function(){
+        var i_1 = Number(newnumber);
+		var i_2 = Number(number);
+		var answer = 0;
+		if (operator == "+") {
+		    answer = i_1 + i_2;
+		} else if (operator == "-") {
+		    answer = i_1 - i_2;
+		} else if (operator == "/") {
+		    answer = i_1 / i_2;
+		} else if (operator == "*") {
+		    answer = i_1 * i_2;
+		}
+		totaldiv.text(answer.toString());
+		testNumLength(answer);
+		number = "";
+		newnumber = number;
     });
 });
